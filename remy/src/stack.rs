f24a03f6a31fa68b7b444cf89199fea66f0f5550
anurse <Andrew Stanton-Nurse>
2015-01-15 17:17:37 -0800

    add a stack abstraction

--- remy/src/stack.rs
+++ remy/src/stack.rs
@@ -69,7 +69,7 @@ impl<'a, A: UnsignedInt+FromPrimitive, M: mem::Memory<A>> Stack<'a, A, M> {
 			let native_sp : usize = try!(NumCast::from(self.sp).ok_or(StackError::StackPointerTooLarge));
 			let new_val = native_sp + size_of::<I>();
 			self.sp = try!(FromPrimitive::from_uint(new_val).ok_or(StackError::StackPointerTooLarge));
-			
+
 			Ok(try!(self.memory.get(self.sp)))
 		}
 	}
@@ -86,13 +86,6 @@ mod test {
 		let mut memory = mem::FixedMemory::with_size(16u8);
 		let mut stack = Stack::with_boundaries(&mut memory, 2, Option::Some(0x2), Option::None);
 		assert_eq!(stack.push(10).unwrap_err(), StackError::ReachedPushBoundary);
-	}
-
-	#[test]
-	pub fn pop_returns_error_if_at_push_boundary() {
-		let mut memory = mem::FixedMemory::with_size(16u8);
-		let mut stack = Stack::with_boundaries(&mut memory, 2, Option::None, Option::Some(0x2));
-		assert_eq!(stack.pop::<u8>().unwrap_err(), StackError::ReachedPopBoundary);
 	}
 
 	#[test]
@@ -201,5 +194,120 @@ mod test {
 
 		assert!(stack.push(75536i32).is_ok());
 		assert_eq!(stack.sp, 11);
+	}
+
+	#[test]
+	pub fn pop_returns_error_if_at_push_boundary() {
+		let mut memory = mem::FixedMemory::with_size(16u8);
+		let mut stack = Stack::with_boundaries(&mut memory, 2, Option::None, Option::Some(0x2));
+		assert_eq!(stack.pop::<u8>().unwrap_err(), StackError::ReachedPopBoundary);
+	}
+
+	#[test]
+	pub fn pop_u8_reads_u8_value_from_next_sp_location() {
+		let mut memory = mem::FixedMemory::with_size(16u8); // Make a 16-byte memory
+		memory.set(1, 42u8);
+		let mut stack = Stack::new(&mut memory, 0); // Start the stack at the end of memory
+
+		assert_eq!(stack.pop::<u8>().unwrap(), 42);
+	}
+
+	#[test]
+	pub fn pop_u8_moves_stack_pointer_up_1_byte() {
+		let mut memory = mem::FixedMemory::with_size(16u8); // Make a 16-byte memory
+		let mut stack = Stack::new(&mut memory, 0); // Start the stack at the end of memory
+
+		assert!(stack.pop::<u8>().is_ok());
+		assert_eq!(stack.sp, 1);
+	}
+
+	#[test]
+	pub fn pop_i8_reads_i8_value_from_next_sp_location() {
+		let mut memory = mem::FixedMemory::with_size(16u8); // Make a 16-byte memory
+		memory.set(1, -42i8);
+		let mut stack = Stack::new(&mut memory, 0); // Start the stack at the end of memory
+
+		assert_eq!(stack.pop::<i8>().unwrap(), -42);
+	}
+
+	#[test]
+	pub fn pop_i8_moves_stack_pointer_up_1_byte() {
+		let mut memory = mem::FixedMemory::with_size(16u8); // Make a 16-byte memory
+		let mut stack = Stack::new(&mut memory, 0); // Start the stack at the end of memory
+
+		assert!(stack.pop::<i8>().is_ok());
+		assert_eq!(stack.sp, 1);
+	}
+
+	#[test]
+	pub fn pop_u16_reads_u16_value_from_next_sp_location() {
+		let mut memory = mem::FixedMemory::with_size(16u8); // Make a 16-byte memory
+		memory.set(2, 1024u16);
+		let mut stack = Stack::new(&mut memory, 0); // Start the stack at the end of memory
+
+		assert_eq!(stack.pop::<u16>().unwrap(), 1024);
+	}
+
+	#[test]
+	pub fn pop_u16_moves_stack_pointer_up_2_bytes() {
+		let mut memory = mem::FixedMemory::with_size(16u8); // Make a 16-byte memory
+		let mut stack = Stack::new(&mut memory, 0); // Start the stack at the end of memory
+
+		assert!(stack.pop::<u16>().is_ok());
+		assert_eq!(stack.sp, 2);
+	}
+
+	#[test]
+	pub fn pop_i16_reads_i16_value_from_next_sp_location() {
+		let mut memory = mem::FixedMemory::with_size(16u8); // Make a 16-byte memory
+		memory.set(2, -1024i16);
+		let mut stack = Stack::new(&mut memory, 0); // Start the stack at the end of memory
+
+		assert_eq!(stack.pop::<i16>().unwrap(), -1024);
+	}
+
+	#[test]
+	pub fn pop_i16_moves_stack_pointer_up_2_bytes() {
+		let mut memory = mem::FixedMemory::with_size(16u8); // Make a 16-byte memory
+		let mut stack = Stack::new(&mut memory, 0); // Start the stack at the end of memory
+
+		assert!(stack.pop::<i16>().is_ok());
+		assert_eq!(stack.sp, 2);
+	}
+
+	#[test]
+	pub fn pop_u32_reads_u32_value_from_next_sp_location() {
+		let mut memory = mem::FixedMemory::with_size(16u8); // Make a 16-byte memory
+		memory.set(4, 75536u32);
+		let mut stack = Stack::new(&mut memory, 0); // Start the stack at the end of memory
+
+		assert_eq!(stack.pop::<u32>().unwrap(), 75536);
+	}
+
+	#[test]
+	pub fn pop_u32_moves_stack_pointer_up_4_bytes() {
+		let mut memory = mem::FixedMemory::with_size(16u8); // Make a 16-byte memory
+		let mut stack = Stack::new(&mut memory, 0); // Start the stack at the end of memory
+
+		assert!(stack.pop::<u32>().is_ok());
+		assert_eq!(stack.sp, 4);
+	}
+
+	#[test]
+	pub fn pop_i32_reads_i32_value_from_next_sp_location() {
+		let mut memory = mem::FixedMemory::with_size(16u8); // Make a 16-byte memory
+		memory.set(4, -75536i32);
+		let mut stack = Stack::new(&mut memory, 0); // Start the stack at the end of memory
+
+		assert_eq!(stack.pop::<i32>().unwrap(), -75536);
+	}
+
+	#[test]
+	pub fn pop_i32_moves_stack_pointer_up_4_bytes() {
+		let mut memory = mem::FixedMemory::with_size(16u8); // Make a 16-byte memory
+		let mut stack = Stack::new(&mut memory, 0); // Start the stack at the end of memory
+
+		assert!(stack.pop::<i32>().is_ok());
+		assert_eq!(stack.sp, 4);
 	}
 }

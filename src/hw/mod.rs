0568eb2cdacfe7a84e721bbe1e1a57f19583ddbb
anurse <andrew@andrewnurse.net>
2015-05-15 17:51:44 -0700

    add simple structure for holding Tiles

--- src/hw/mod.rs
+++ src/hw/mod.rs
@@ -4,3 +4,10 @@
 /// (in the form of the slightly modified Ricoh 2A03), the [Apple II](http://en.wikipedia.org/wiki/Apple_II), 
 /// [8-bit Atari units](http://en.wikipedia.org/wiki/Atari_8-bit_family), and the [Commodore 64](http://en.wikipedia.org/wiki/Commodore_64)
 pub mod mos6502;
+
+/// Provides emulation for the [Ricoh RP2C02 Picture Processing Unit (PPU)](https://en.wikipedia.org/wiki/Picture_Processing_Unit) as well
+/// as the RP2C07 (for PAL television systems)
+///
+/// This processor is found in the [Nintendo Entertainment System](http://en.wikipedia.org/wiki/Nintendo_Entertainment_System)
+#[allow(non_snake_case)]
+pub mod rp2C02;
---
+++ src/hw/rp2C02/mod.rs
@@ -0,0 +1,2 @@
+/// Contains code to represent the Pattern Table and Tiles
+pub mod pattern_table;
